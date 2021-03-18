e9545444efb6f9b46313bb134eaca0c2ccf27694
ngwatso <ngwatson79@yahoo.com>
2021-03-17 22:33:00 -0400

    progress

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-const server = require("./api/server.js");
+require('dotenv').config();
+const server = require('./api/server.js');
 
-const port = 5000;
+const port = process.env.PORT;
 server.listen(port, () => {
-  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
+	console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
+
+// ?? CI (Continuous Intergration) / CD (Continuos Deployment)
+// ?? DEV ==> TEST ==> STAGING ==> PRODUCTION
+
+// ** SET PORT
+// ?? export PORT=4040
+// ?? unset PORT
